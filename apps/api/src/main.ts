37dadd27f30e6d68ebb94ee7c383f38b88cd7257
Joshua Saunders <jk.saunders@hotmail.com>
2022-08-01 22:46:29 -0600

    Added fastify + mercurius

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,32 @@
 
 import * as express from 'express';
 
+import Fastify from 'fastify';
+import mercurius from 'mercurius';
+
+const fastifyApp = Fastify({ logger: true });
+
+const schema = `
+  type Query {
+    add(x: Int, y: Int): Int
+  }
+`;
+
+const resolvers = {
+  Query: {
+    add: async (_, { x, y }) => x + y,
+  },
+};
+
+fastifyApp.register(mercurius, {
+  schema,
+  resolvers,
+  context: (request, reply) => {
+    console.log('context test');
+    return {};
+  },
+});
+
 const app = express();
 
 app.get('/api', (req, res) => {
